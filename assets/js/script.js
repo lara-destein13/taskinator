24fb57b3d796ecfe75b81b6cdcff362fbd36ee2f
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-31 14:15:35 -0500

    get create new task button and feature working

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,6 +69,12 @@ var tasksToDoEl = document.querySelecto("#tasks-to-do");
 
 
 var createTaskHandler = function() {
-
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  };
+  
+  buttonEl.addEventListener("click", createTaskHandler);
 }
 
